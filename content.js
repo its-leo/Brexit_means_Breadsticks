fb70e774197459daa004a72cc3707f82c8a0b899
Lennard <hensel.lennard@live.com>
2021-05-21 19:53:01 +0200

    initial commit

--- content.js
+++ content.js
@@ -1,9 +1,81 @@
-/*jshint esversion: 6 */
-
-
 var elementsInsideBody = [...document.body.getElementsByTagName('*')];
-// This makes an array of everything inside the body tag
 
+var incomingList = [{
+  f: "Brexit",
+  t: ["Breadsticks"]
+},{
+  f: "Bernd Höcke",
+  t: ["Björn Höcke"]
+},{
+  f: "AKK",
+  t: ["AKK-47", "AKKKK", "CDUzi"]
+},{
+  f: /(?:Annegret )?Kramp[ -]Karrenbauer/gi,
+  t: ["Annegret Kampf-Knarrenbauer", "Annegret Kramp-Karrenbauer-Kramer-Korbklemmer"]
+},{
+  f: /(?:Julia )?Klöckner/gi,
+  t: ["Julia Cini-Mini","Julia Nesquik","Julia Nescafe","Julia Nespresso", "Die Weinkönigin"]
+},{
+  f: /(?:Armin )?Laschet/gi,
+  t: ["Würfel-Armin"]
+},{
+  f: /(?:Olaf )?Scholz/gi,
+  t: ["Giro-Olaf"]
+},{
+  f: /(?:Donald )?Trump/gi,
+  t: ["Florida Man "]
+},{
+  f: "Kanzler",
+  t: ["Imperator"]
+},{
+  f: /(?:Partei)/gi,
+  t: ["Klapsmühle"]
+},{
+  f: "Politiker",
+  t: ["Steuerinvestor", "Produktivitätsoptimierer"]
+},{
+  f: "Bundesminister",
+  t: ["Lobbykorrespondent", "Unternehmensvertreter"]
+},{
+  f: /(?:Andreas |Andi |Andy )?[S]{1}cheuer/gi,
+  t: ["Roller-Andi","Andi B. Scheuert", "Stullen-Andi"]
+},{
+  f: "DDR",
+  t: ["Bananen-freie Zone"]
+},{
+  f: "Österreich",
+  t: ["Hinter Bayern"]
+},{
+  f: "Kirche",
+  t: ["Kirsche","Bingo-Gruppe"]
+},{
+  f: "Christlich",
+  t: ["Moralisch-flexibel"]
+},{
+  f: "Englisch",
+  t: ["Angelsächsisch"]
+},{ 
+  f: /(?:Winfried )?Kretschmann/gi,
+  t: ["Er"]
+},{
+  f: /(?:Angela )?Merkel/gi,
+  t: ["Mutti"]
+},{
+  f: /(?:Karl-Theodor zu )?Guttenberg/gi,
+  t: ["Der Kopierer"]
+},{
+  f: "Wahlkampf",
+  t: ["Sackhüpfwettbewerb"]
+},{ 
+  f: /(?:Annalena )?Baerbock/gi,
+  t: ["Die Annalena", "Annalena Bärbaum"]
+}]
+
+
+function chooseRandom(){
+
+}
+// This makes an array of everything inside the body tag
 
 //a function that loops through every single item
 function findAndReplace(){
@@ -13,14 +85,18 @@ function findAndReplace(){
         replaceText(child);
       }
     });
-
   });
 }
 
 function replaceText (node) {
   let value = node.nodeValue;
-  value = value.replace(/Brexit/gi, 'Breadsticks');
-  value = value.replace(/brexit/gi, 'breadsticks');
+let item
+
+incomingList.forEach(element =>{
+    item = element.t[Math.floor(Math.random() * element.t.length)];
+    value = value.replace(element.f, item)
+  }); 
+ 
   node.nodeValue = value;
 }
 
